1782afe179ee14de7b0c5fd69c91da6d749fb145
Ashwin045 <23203003@srcas.ac.in>
2024-07-01 12:48:01 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -2,10 +2,10 @@ class sample{
 public static void main(String[]args){
 for(int i=1;i<=10;i++){
   if(i%2==0){
-    System.out.println(i);
+    System.out.println("Even number:"+i);
   }
   else{
-    System.out.println("not even");
+    System.out.println("Odd Number:"+i);
   }
 }
 }
